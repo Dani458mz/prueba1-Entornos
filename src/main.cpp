2d7a6a86e3bdaecefc0df561ccea2e019fd7b02f
Daniel <dmarzaf634@g.educaand.es>
2021-12-20 09:11:03 +0100

    Agregamos toda la informacion del main

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1,n2,n3;
+
+    cout <<"Introduce el primer número" <<endl;
+    cin >> n1 ;
+    cout <<"Introduce el segundo número" <<endl;
+    cin >> n2 ;
+
+    n3=n1 + n2;
+
+    cout <<"La suma es:" << n3 <<endl;
+
+    return 0;
+}
